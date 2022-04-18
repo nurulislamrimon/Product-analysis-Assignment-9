1d51aadb962a522b7682f135ad2e2f8e32b31ca5
nurulislamrimon <nurulislamrimon@gmail.com>
2022-04-19 00:41:39 +0600

    Dashboard done

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -5,7 +5,7 @@ import Review from './Review';
 const Reviews = () => {
     const [reviews, setReviews] = useReviewsData([]);
     return (
-        <div className=' grid md:grid-cols-3'>
+        <div className=' grid lg:grid-cols-4 md:grid-cols-3'>
             {reviews.map(review => <Review
                 key={review._id}
                 review={review} ></Review>)}
